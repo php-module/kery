0fc5dabe52e68dc647da6939fe3f3943611f6b9f
agostinhosamil <agostinhosaml832@gmail.com>
2021-07-23 10:42:20 +0100

    feat(Kery-dialects): create MySQL WildCards Extension

---
+++ Sammy/Packs/Kery/Dialects/MySQL/Ext/WildCard/Order.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * @version 2.0
+ * @author Sammy
+ *
+ * @keywords Samils, ils, php framework
+ * -----------------
+ * @package Sammy\Packs\Kery\Dialects\MySQL\Ext\WildCard
+ * - Autoload, application dependencies
+ *
+ * MIT License
+ *
+ * Copyright (c) 2020 Ysare
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+namespace Sammy\Packs\Kery\Dialects\MySQL\Ext\WildCard {
+  /**
+   * Make sure the module base internal class is not
+   * declared in the php global scope defore creating
+   * it.
+   * It ensures that the script flux is not interrupted
+   * when trying to run the current command by the cli
+   * API.
+   */
+  if (!trait_exists('Sammy\Packs\Kery\Dialects\MySQL\Ext\WildCard\Order')){
+  /**
+   * @trait Order
+   * Base internal trait for the
+   * Kery\Dialects\MySQL\Ext\WildCard module.
+   * -
+   * This is (in the ils environment)
+   * an instance of the php module,
+   * wich should contain the module
+   * core functionalities that should
+   * be extended.
+   * -
+   * For extending the module, just create
+   * an 'exts' directory in the module directory
+   * and boot it by using the ils directory boot.
+   * -
+   */
+  trait Order {
+    /**
+     * @method string readOrderWildCard
+     *
+     */
+    private static function readOrderWildCard ($orders) {
+      if (!(is_array ($orders) && $orders)) {
+        return '';
+      }
+
+      /**
+       * [$orders_str description]
+       * @var [type]
+       */
+      $orders_str = ' order by ';
+
+      foreach ($orders as $f => $o) {
+        if (is_int ($f)) {
+          $field = (string) ($o);
+          $order = 'asc';
+        } else {
+          $field = (string) ($f);
+          $order = (string) ($o);
+        }
+
+        $orders_str .= '`'.$field.'` ' . $order . ', ';
+      }
+
+      return preg_replace ('/(,\s*)$/', '', $orders_str);
+    }
+  }}
+}
